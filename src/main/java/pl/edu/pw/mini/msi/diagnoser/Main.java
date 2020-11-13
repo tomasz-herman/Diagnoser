c6f24221802d9f8f5138caee529891374eb9cc8e
Tomasz Herman <tomasz.herman@protonmail.com>
2020-11-13 21:54:16 +0100

    Fixed JavaFX

--- src/main/java/pl/edu/pw/mini/msi/diagnoser/Main.java
+++ src/main/java/pl/edu/pw/mini/msi/diagnoser/Main.java
@@ -1,21 +1,7 @@
 package pl.edu.pw.mini.msi.diagnoser;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
+public class Main {
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
-        primaryStage.setScene(new Scene(root));
-        primaryStage.getScene().getStylesheets().add("/main.css");
-        primaryStage.show();
+        MainPanel.launch(args);
     }
 }
